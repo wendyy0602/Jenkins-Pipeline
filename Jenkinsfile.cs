435e673768d4f6b6d799728938f61be022065001
wendyy0602 <wendy.lam0602@gmail.com>
2024-09-17 01:15:26 +1000

    Add Jenkinsfile

---
+++ Jenkinsfile.cs
@@ -0,0 +1,17 @@
+﻿pipeline{
+	agent any
+	stages{
+		stage("Build"){
+			steps{
+				echo "Building ..."
+			}
+			post{
+				success{
+					mail to: "wendy.lam0602@gmail.com",
+					subject: "Build Status Email",
+					body: "Build was successful!"
+				}
+			}
+		}
+	}
+}
